c29856817265fb0b5b533743c6cc159760098da7
LuciaFores <77293908+LuciaFores@users.noreply.github.com>
2023-04-11 10:54:24 +0200

    Divided codes into different folders
    - Created folder for model training container
    - Created folder for webapp container

---
+++ webapp/webapp.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pickle
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.applications.resnet50 import preprocess_input
+import keras.utils as image
+
+# Title
+st.header("Cloud Computing Machine Learning App")
+
+# File Uploader
+img = st.file_uploader("Image to be recognized", type=['png', 'jpg', 'jpeg'])
+
+# If submit is pressed
+if st.button("Submit"):
+    # CIFAR-10 labels
+    labels = [
+    "airplane",
+    "automobile",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck"
+    ]
+    # Image pre-processing
+    # 1. Image resizing
+    res_img = image.load_img(img, target_size=(32,32))
+    # 2. Batching the image
+    img_array = image.img_to_array(res_img)
+    img_batch = np.expand_dims(img_array, axis=0)
+    # 3. Consistent pixels
+    img_preprocessed = preprocess_input(img_batch)
+    # Unpickle the model
+    model = pickle.load(open('finalized_model.sav', 'rb'))
+    prediction = labels[model.predict(img_preprocessed).argmax()]
+    # Output the prediction
+    st.text(f"This image represents a {prediction}")
