91499b794233e16c2b659ef56a801b8f49fcb81a
jngelena <jngelena@gmail.com>
2023-05-18 21:19:00 +0200

    update flask webapp

--- webapp_flask/webapp.py
+++ webapp_flask/webapp.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request
 from PIL import Image #pip3 install Pillow
 import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras.applications.resnet50 import preprocess_input #pip3 install tensorflow keras
 import numpy as np
 import pickle
-
 
 app = Flask(__name__)
 
@@ -19,7 +17,7 @@ def allowed_file(filename):
 def index():
     return render_template('index.html')
 
-@app.route('/success', methods=['POST'])
+@app.route('/', methods=['POST'])
 def success():
     if request.method == 'POST': 
         file = request.files['file']
@@ -38,7 +36,6 @@ def success():
         ]
 
         if file and allowed_file(file.filename):
-            #f.save(f.filename)
             img = Image.open(file)
             img = img.resize((32, 32))
             img_array = np.array(img)
@@ -46,15 +43,12 @@ def success():
             img_preprocessed = preprocess_input(img_batch)
             
             model = pickle.load(open("finalized_model.sav", 'rb'))
-        
             prediction = labels[model.predict(img_preprocessed).argmax()]
 
-            return render_template('success.html', name=prediction)
-            
-
+            return render_template('index.html', result = "Prediction Result", 
+                                   prediction = f"This image represents a {prediction}")
         else:
-            return "Invalid file type. Please upload a PNG, JPG or JPEG file."
-    
+            return render_template('index.html', prediction="Invalid file type. Please upload a PNG, JPG or JPEG file.")
 
 if __name__ == '__main__':
     app.run()
